5a07cde28428e6d4dbc4fa4cc773a3aa1c7f510b
Alexander Clouter <aclouter@telemetry.com>
2014-10-28 22:24:07 +0000

    remove a pile of junk and enable linux-atm

--- board/qemu/mipsel/post-build.sh
+++ board/qemu/mipsel/post-build.sh
@@ -7,17 +7,61 @@ if [ $# -ne 1 ] || [ -z "$1" ]; then
 	exit 1
 fi
 
+ROOTDIR="$1"
+
 # workaround missing libgcc_s.so
-cp output/host/usr/mipsel-buildroot-linux-uclibc/sysroot/lib/libgcc_s.so.1 "$1/lib"
-output/host/usr/mipsel-buildroot-linux-uclibc/bin/strip "$1/lib/libgcc_s.so.1"
-ln -f -s libgcc_s.so.1 "$1/lib/libgcc_s.so"
+cp output/host/usr/mipsel-buildroot-linux-uclibc/sysroot/lib/libgcc_s.so.1 "$ROOTDIR/lib"
+output/host/usr/mipsel-buildroot-linux-uclibc/bin/strip "$ROOTDIR/lib/libgcc_s.so.1"
+ln -f -s libgcc_s.so.1 "$ROOTDIR/lib/libgcc_s.so"
 
-rm -f "$1/root/.bash_history"
-rm -f "$1/root/.bash_logout"
-rm -f "$1/root/.bash_profile"
+find "$ROOTDIR/usr/lib/pppd/2.4.7" -type f ! -name pppoatm.so ! -name rp-pppoe.so -delete
 
-find "$1/var" -depth -mindepth 1 | xargs -r rm -r
+FILES="	sbin/hediag
+	sbin/lecs
+	sbin/atmsigd
+	sbin/pppoe-discovery
+	sbin/zntune
+	sbin/pppstats
+	sbin/atmarp
+	sbin/ilmidiag
+	sbin/bus
+	sbin/mpcd
+	sbin/esi
+	sbin/atmarpd
+	sbin/atmloop
+	sbin/chat
+	sbin/zeppelin
+	sbin/ilmid
+	sbin/pppdump
+	sbin/les
+	sbin/enitune
+	sbin/atmaddr
+	bin/awrite
+	bin/svc_recv
+	bin/sonetdiag
+	bin/ngettext
+	bin/atmdiag
+	bin/atmswitch
+	bin/gettext
+	bin/svc_send
+	bin/envsubst
+	bin/saaldump
+	bin/atmdump
+	bin/ttcp_atm
+	bin/aread
+	bin/gettext.sh"
 
-find "$1/etc/nftables" -type f ! -name inet-filter -delete
+echo "$FILES" | xargs -I{} rm -f "$ROOTDIR/usr/{}"
+
+find "$ROOTDIR/var" -depth -mindepth 1 | xargs -r rm -r
+
+find "$ROOTDIR/etc/nftables" -type f ! -name inet-filter -delete
+
+rm -rf "$ROOTDIR/usr/lib32"
+rm -rf "$ROOTDIR/usr/share/locale"
+
+rm -f "$ROOTDIR/root/.bash_history"
+rm -f "$ROOTDIR/root/.bash_logout"
+rm -f "$ROOTDIR/root/.bash_profile"
 
 exit 0
