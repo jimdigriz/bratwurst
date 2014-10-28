96d17abaa67636b7400cc2217f97ef5685044563
Alexander Clouter <aclouter@telemetry.com>
2014-10-28 21:29:57 +0000

    turns out libgcc_so is needed

--- board/qemu/mipsel/post-build.sh
+++ board/qemu/mipsel/post-build.sh
@@ -7,6 +7,11 @@ if [ $# -ne 1 ] || [ -z "$1" ]; then
 	exit 1
 fi
 
+# workaround missing libgcc_s.so
+cp output/host/usr/mipsel-buildroot-linux-uclibc/sysroot/lib/libgcc_s.so.1 "$1/lib"
+output/host/usr/mipsel-buildroot-linux-uclibc/bin/strip "$1/lib/libgcc_s.so.1"
+ln -f -s libgcc_s.so.1 "$1/lib/libgcc_s.so"
+
 rm "$1/root/.bash_history"
 rm "$1/root/.bash_logout"
 rm "$1/root/.bash_profile"
