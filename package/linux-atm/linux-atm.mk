0c04595cadeb59d8a8c5b8689f2e10554a01416b
Alexander Clouter <aclouter@telemetry.com>
2014-10-28 21:44:29 +0000

    added linux-atm

---
+++ package/linux-atm/linux-atm.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# linux-atm
+#
+################################################################################
+
+LINUX_ATM_VERSION = 2.5.2
+LINUX_ATM_SOURCE = linux-atm-$(LINUX_ATM_VERSION).tar.gz
+LINUX_ATM_SITE = http://sourceforge.net/projects/linux-atm/files
+LINUX_ATM_INSTALL_STAGING = YES
+LINUX_ATM_LICENSE = GPLv2
+LINUX_ATM_LICENSE_FILES = COPYING
+
+IPSEC_TOOLS_CONF_OPT = --with-kernel-headers=$(STAGING_DIR)/usr/include
+
+$(eval $(autotools-package))
