3206a54454bf5f195b9989b3b17eee7d12e3f8f9
Alexander Clouter <alex@digriz.org.uk>
2014-12-14 18:47:36 +0000

    use latest git version of nftables

--- package/external-nftables/nftables.mk
+++ package/external-nftables/nftables.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-EXTERNAL_NFTABLES_VERSION = 0.3
-EXTERNAL_NFTABLES_SOURCE = nftables-$(EXTERNAL_NFTABLES_VERSION).tar.bz2
-EXTERNAL_NFTABLES_SITE = http://www.netfilter.org/projects/nftables/files
-EXTERNAL_NFTABLES_DEPENDENCIES = gmp libmnl libnftnl host-bison host-flex \
+EXTERNAL_NFTABLES_VERSION = a698868d52a550bab4867c0dc502037155baa11d
+EXTERNAL_NFTABLES_SITE = git://git.netfilter.org/nftables
+EXTERNAL_NFTABLES_DEPENDENCIES = gmp external-libmnl external-libnftnl host-bison host-flex \
 	host-pkgconf $(if $(BR2_NEEDS_GETTEXT),gettext)
 EXTERNAL_NFTABLES_AUTORECONF = YES
 EXTERNAL_NFTABLES_LICENSE = GPLv2
