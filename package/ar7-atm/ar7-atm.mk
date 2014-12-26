51bbdc17ace0108359f6eab610e74ff2871b025d
Alexander Clouter <alex@digriz.org.uk>
2014-12-26 12:19:39 +0000

    ar7-atm copy firmware into place

--- package/ar7-atm/ar7-atm.mk
+++ package/ar7-atm/ar7-atm.mk
@@ -12,11 +12,12 @@ AR7_ATM_LICENSE =
 AR7_ATM_DEPENDENCIES = linux
 
 define AR7_ATM_BUILD_CMDS
-        $(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D)
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D)
 endef
 
 define AR7_ATM_INSTALL_TARGET_CMDS
-        $(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D) modules_install
+	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D) modules_install
+	$(INSTALL) -D $(@D)/ar0700mp.bin $(TARGET_DIR)/lib/firmware/ar0700xx.bin
 endef
 
 $(eval $(generic-package))
