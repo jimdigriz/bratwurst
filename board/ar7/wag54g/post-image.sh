9a9e3acc76dcfd6767ef0991c892f63f0a202a95
Alexander Clouter <alex@digriz.org.uk>
2014-12-28 15:30:12 +0000

    does not need to be a fullsized image

--- board/ar7/wag54g/post-image.sh
+++ board/ar7/wag54g/post-image.sh
@@ -20,11 +20,11 @@ if [ $(cat rootfs.jffs2 | wc -c) -gt $((3136*1024)) ]; then
 	exit 1
 fi
 
-DD_PARAMS="status=noxfer of=firmware-code.bin.headerless count=1"
+DD_PARAMS="status=noxfer of=firmware-code.bin.headerless"
 
-dd $DD_PARAMS conv=sync                      bs=16    if=/dev/zero	2>/dev/null
-dd $DD_PARAMS conv=sync,notrunc oflag=append bs=768k  if=vmlinuz.bin	2>/dev/null
-dd $DD_PARAMS conv=sync,notrunc oflag=append bs=3136k if=rootfs.jffs2	2>/dev/null
+dd $DD_PARAMS conv=sync                      count=1 bs=16   if=/dev/zero	2>/dev/null
+dd $DD_PARAMS conv=sync,notrunc oflag=append count=1 bs=768k if=vmlinuz.bin	2>/dev/null
+dd $DD_PARAMS conv=sync,notrunc oflag=append                 if=rootfs.jffs2	2>/dev/null
 
 cat firmware-code.bin.headerless | ../host/usr/bin/addpattern -o .firmware-code.bin -p WA21
 
