7657f4b03c24fd4f2d6373ffc7a71fc848c67851
Alexander Clouter <alex@digriz.org.uk>
2015-01-18 15:32:01 +0000

    bump odhcp6c

--- package/odhcp6c/odhcp6c.mk
+++ package/odhcp6c/odhcp6c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ODHCP6C_VERSION = 722226c4f1d45c8bf4ac9189523738abcf7d648f
+ODHCP6C_VERSION = c42e34111e19bd435dc4b5bb3ba81224ea214314
 ODHCP6C_SITE = $(call github,sbyx,odhcp6c,$(ODHCP6C_VERSION))
 ODHCP6C_LICENSE = GPLv2
 ODHCP6C_LICENSE_FILES = COPYING
