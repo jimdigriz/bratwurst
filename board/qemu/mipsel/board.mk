bcc3de2f3d62e87c4f651aa4645345604859ba25
Alexander Clouter <alex@digriz.org.uk>
2015-04-12 10:24:24 +0100

    avoid 'clean' in the 'make clean bratwurst' dev cycle

--- board/qemu/mipsel/board.mk
+++ board/qemu/mipsel/board.mk
@@ -3,5 +3,11 @@ NAME-qemu/mipsel	:= QEMU mipsel system
 
 .PHONY:	qemu/mipsel
 qemu/mipsel: BOARD	:= qemu/mipsel
-qemu/mipsel: world
+qemu/mipsel: | qemu/mipsel-remove-pflash world
 	echo mipsel > .qemu-arch
+
+# so we can avoid the slower dev path 'make clean bratwurst'
+.PHONY: qemu/mipsel-remove-pflash
+qemu/mipsel-remove-pflash:
+	rm -f $(PFLASH)
+	rsync -qrl rootfs/ buildroot/output/target/
