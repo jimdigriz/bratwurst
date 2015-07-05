f9067d51dc5a3e8e8f6c2f40caff151269512c4c
Alexander Clouter <alex@digriz.org.uk>
2015-07-05 16:09:44 +0100

    kernel boots

--- board/lantiq/td-w8980/post-image.sh
+++ board/lantiq/td-w8980/post-image.sh
@@ -4,19 +4,20 @@ set -eu
 
 cd "$1"
 
-xz --format=lzma -c vmlinux.bin > .vmlinux.bin.lzma
-mv .vmlinux.bin.lzma vmlinux.bin.lzma
+if [ $(cat rootfs.jffs2 | wc -c) -gt $((0x7a0000 - 0x140000 - 512)) ]; then
+	echo rootfs.jffs2 is too large >&2
+	exit 1
+fi
 
-#if [ $(cat vmlinux.bin.lzma | wc -c) -gt $((768*1024)) ]; then
-#	echo vmlinux.bin.lzma is too large >&2
-#	exit 1
-#fi
+xz --format=lzma --stdout --lzma1=lc=1,lp=2,pb=2 vmlinux.bin > vmlinux.bin.lzma
 
-#if [ $(cat rootfs.jffs2 | wc -c) -gt $((3136*1024)) ]; then
-#	echo rootfs.jffs2 is too large >&2
-#	exit 1
-#fi
+if [ $(cat vmlinux.bin.lzma | wc -c) -gt $((0x140000 - 512)) ]; then
+	echo vmlinux.bin.lzma is too large >&2
+	exit 1
+fi
 
-../host/usr/bin/mktplinkfw2 -B TD-W8970v1 -s -a 0x4 -j -k vmlinux.bin.lzma -r rootfs.jffs2 -o bratwurst.image
+ENTRY=$(../host/usr/mips-buildroot-linux-uclibc/bin/nm ../build/linux-$(sed -n 's/BR2_LINUX_KERNEL_VERSION="\(.*\)"/\1/ p' ../../.config)/vmlinux | awk '{ if ($3 == "kernel_entry") print $1 }')
+
+../host/usr/bin/mktplinkfw2 -c -B TD-W8970v1 -s -k vmlinux.bin.lzma -E 0x$ENTRY -o bratwurst.uImage
 
 exit 0
