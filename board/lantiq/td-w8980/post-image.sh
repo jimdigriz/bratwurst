51907563d1f37b0a719845a7e9b13d351a71385a
Alexander Clouter <alex@digriz.org.uk>
2015-08-23 13:46:57 +0100

    path nm tweak

--- board/lantiq/td-w8980/post-image.sh
+++ board/lantiq/td-w8980/post-image.sh
@@ -17,7 +17,7 @@ if [ $(cat vmlinux.bin.lzma | wc -c) -gt $((0x140000)) ]; then
 	exit 1
 fi
 
-ENTRY=$(../host/usr/mips-buildroot-linux-uclibc/bin/nm ../build/linux-$(sed -n 's/BR2_LINUX_KERNEL_VERSION="\(.*\)"/\1/ p' ../../.config)/vmlinux | awk '{ if ($3 == "kernel_entry") print $1 }')
+ENTRY=$(../host/usr/bin/mips-buildroot-linux-uclibc-nm ../build/linux-$(sed -n 's/BR2_LINUX_KERNEL_VERSION="\(.*\)"/\1/ p' ../../.config)/vmlinux | awk '{ if ($3 == "kernel_entry") print $1 }')
 
 ../host/usr/bin/mktplinkfw2 -H 0x89800001 -F 8Mltq -s -E 0x$ENTRY \
 	-k vmlinux.bin.lzma -r rootfs.jffs2 -o bratwurst.image
